091761aac9025f87262cedf053a731d45385a6ca
sweetflavor <sweet.devtools@gmail.com>
2019-01-14 13:07:18 +0000

    Image
    Adding the logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,23 @@
 <template>
   <section>
     <div>
-      <logo/>
-      <h1 class="title">NUXT</h1>
-      <h2 class="subtitle">
-        Starter for CodeSandBox
-      </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation <icon-link/></a>
-        <nuxt-link
-          to="/about"
-          class="button--grey">About</nuxt-link>
+        <img src="../assets/codesandbox_icon.png" width="120" alt="" />
+        <h1>Hola a Todos!</h1>
+        <nuxt-link to="/about" class="button--grey">About</nuxt-link>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import IconLink from '~/components/IconLink.vue'
-
-export default {
-  components: {
-    Logo,
-    IconLink
-  }
-}
+export default {};
 </script>
 
 <style scoped>
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 400;
   font-size: 100px;
